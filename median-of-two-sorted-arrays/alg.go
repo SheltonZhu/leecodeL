9ddb1b05e120a5afa6a27603297cecd36ad807fe
SheltonZhu <498220739@qq.com>
2021-12-18 21:25:38 +0800

    hang up

--- median-of-two-sorted-arrays/alg.go
+++ median-of-two-sorted-arrays/alg.go
@@ -1,6 +1,22 @@
 package median_of_two_sorted_arrays
 
+func Max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+func Min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
-
+	//nums1Len := len(nums1)
+	//nums2Len := len(nums2)
+	//totalLen := nums1Len + nums2Len
+	//isOdd := totalLen&1 == 1
+	//midLen := totalLen / 2
 	return 0
 }
