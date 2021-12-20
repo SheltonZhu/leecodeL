a41cf4feb39bba75a31280e8873053624bc16c18
SheltonZhu <498220739@qq.com>
2021-12-20 21:49:59 +0800

    15

--- 15.3-sum/alg.go
+++ 15.3-sum/alg.go
@@ -1,9 +1,41 @@
 package _5_3_sum
 
-func threeSum(nums []int) [][]int {
-	return [][]int{}
-}
+import "sort"
 
-func ThreeSum(nums []int) {
-	threeSum(nums)
+func threeSum(nums []int) [][]int {
+	if len(nums) < 3 {
+		return [][]int{}
+	}
+	sort.Ints(nums)
+	left, right := 0, 0
+	var res [][]int
+	for i := 0; i < len(nums); i++ {
+		if nums[i] > 0 {
+			return res
+		}
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		left = i + 1
+		right = len(nums) - 1
+		for left < right {
+			sum := nums[i] + nums[left] + nums[right]
+			if sum == 0 {
+				res = append(res, []int{nums[left], nums[i], nums[right]})
+				for left < right && nums[left] == nums[left+1] {
+					left++
+				}
+				for left < right && nums[right] == nums[right-1] {
+					right--
+				}
+				left++
+				right--
+			} else if sum > 0 {
+				right--
+			} else {
+				left++
+			}
+		}
+	}
+	return res
 }
--- 15.3-sum/alg_test.go
+++ 15.3-sum/alg_test.go
@@ -9,7 +9,7 @@ func Test(t *testing.T) {
 	nums := []int{-1, 0, 1, 2, -1, -4}
 	output := [][]int{{-1, -1, 2}, {-1, 0, 1}}
 	ret := threeSum(nums)
-	if reflect.DeepEqual(output, ret) {
+	if !reflect.DeepEqual(output, ret) {
 		t.Errorf("failed. output: %v, return: %v", output, ret)
 	}
 }
@@ -18,7 +18,7 @@ func Test2(t *testing.T) {
 	var nums []int
 	var output [][]int
 	ret := threeSum(nums)
-	if reflect.DeepEqual(output, ret) {
+	if !reflect.DeepEqual(output, ret) {
 		t.Errorf("failed. output: %v, return: %v", output, ret)
 	}
 }
@@ -27,7 +27,7 @@ func Test3(t *testing.T) {
 	nums := []int{0}
 	var output [][]int
 	ret := threeSum(nums)
-	if reflect.DeepEqual(output, ret) {
+	if !reflect.DeepEqual(output, ret) {
 		t.Errorf("failed. output: %v, return: %v", output, ret)
 	}
 }
