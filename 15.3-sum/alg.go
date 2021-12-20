42f8566067c6c5ac58e395d6b6e3bab9d38066e2
sheltonzhu <498220739@qq.com>
2021-12-20 11:34:50 +0800

    ignore

---
+++ 15.3-sum/alg.go
@@ -0,0 +1,9 @@
+package _5_3_sum
+
+func threeSum(nums []int) [][]int {
+	return [][]int{}
+}
+
+func ThreeSum(nums []int) {
+	threeSum(nums)
+}
---
+++ 15.3-sum/alg_test.go
@@ -0,0 +1,33 @@
+package _5_3_sum
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test(t *testing.T) {
+	nums := []int{-1, 0, 1, 2, -1, -4}
+	output := [][]int{{-1, -1, 2}, {-1, 0, 1}}
+	ret := threeSum(nums)
+	if reflect.DeepEqual(output, ret) {
+		t.Errorf("failed. output: %v, return: %v", output, ret)
+	}
+}
+
+func Test2(t *testing.T) {
+	var nums []int
+	var output [][]int
+	ret := threeSum(nums)
+	if reflect.DeepEqual(output, ret) {
+		t.Errorf("failed. output: %v, return: %v", output, ret)
+	}
+}
+
+func Test3(t *testing.T) {
+	nums := []int{0}
+	var output [][]int
+	ret := threeSum(nums)
+	if reflect.DeepEqual(output, ret) {
+		t.Errorf("failed. output: %v, return: %v", output, ret)
+	}
+}
