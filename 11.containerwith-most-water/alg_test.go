b94fa8238a7da84843c61e54ac8abece4ca96cc2
sheltonzhu <498220739@qq.com>
2021-12-20 12:34:24 +0800

    tmp

--- 11.containerwith-most-water/alg_test.go
+++ 11.containerwith-most-water/alg_test.go
@@ -9,7 +9,7 @@ func Test(t *testing.T) {
 	input := []int{1, 8, 6, 2, 5, 4, 8, 3, 7}
 	output := 49
 	ret := maxArea(input)
-	if reflect.DeepEqual(output, ret) {
+	if !reflect.DeepEqual(output, ret) {
 		t.Errorf("failed. output: %v, return: %v", output, ret)
 	}
 }
@@ -18,7 +18,7 @@ func Test2(t *testing.T) {
 	input := []int{1, 1}
 	output := 1
 	ret := maxArea(input)
-	if reflect.DeepEqual(output, ret) {
+	if !reflect.DeepEqual(output, ret) {
 		t.Errorf("failed. output: %v, return: %v", output, ret)
 	}
 }
@@ -27,7 +27,7 @@ func Test3(t *testing.T) {
 	input := []int{4, 3, 2, 1, 4}
 	output := 16
 	ret := maxArea(input)
-	if reflect.DeepEqual(output, ret) {
+	if !reflect.DeepEqual(output, ret) {
 		t.Errorf("failed. output: %v, return: %v", output, ret)
 	}
 }
@@ -36,7 +36,7 @@ func Test4(t *testing.T) {
 	input := []int{1, 2, 1}
 	output := 2
 	ret := maxArea(input)
-	if reflect.DeepEqual(output, ret) {
+	if !reflect.DeepEqual(output, ret) {
 		t.Errorf("failed. output: %v, return: %v", output, ret)
 	}
 }
