6fcaf6378a2aa0487dfe1c8a640dcd6dce6e6eba
SheltonZhu <498220739@qq.com>
2021-12-22 22:08:12 +0800

    39

--- 39.combination-sum/alg.go
+++ 39.combination-sum/alg.go
@@ -1,6 +1,43 @@
 package _9_combination_sum
 
 func combinationSum(candidates []int, target int) [][]int {
-	
-	return [][]int{}
+	var result [][]int
+	//var backtrack func(target int, sum []int)
+	//backtrack = func(target int, sum []int) {
+	//	if target == 0 {
+	//		result = append(result, append([]int{}, sum...))
+	//		return
+	//	}
+	//	if target < 0 {
+	//		return
+	//	}
+	//	for _, item := range candidates {
+	//		if len(sum) == 0 || item >= sum[len(sum)-1] {
+	//			sum = append(sum, item)
+	//			backtrack(target-item, sum)
+	//			sum = sum[:len(sum)-1]
+	//		}
+	//	}
+	//}
+	//
+	//backtrack(target, []int{})
+	backtrack(&result, candidates, target, []int{})
+	return result
+}
+
+func backtrack(result *[][]int, candidates []int, target int, sum []int) {
+	if target == 0 {
+		*result = append(*result, append([]int{}, sum...))
+		return
+	}
+	if target < 0 {
+		return
+	}
+	for _, item := range candidates {
+		if len(sum) == 0 || item >= sum[len(sum)-1] {
+			sum = append(sum, item)
+			backtrack(result, candidates, target-item, sum)
+			sum = sum[:len(sum)-1]
+		}
+	}
 }
