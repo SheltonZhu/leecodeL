66a9ce2652667b13f56bc7d4a0d202f8b24648d4
SheltonZhu <498220739@qq.com>
2021-12-24 00:50:13 +0800

    64+

--- 64.minimum-path-sum/alg.go
+++ 64.minimum-path-sum/alg.go
@@ -7,18 +7,17 @@ func minPathSum(grid [][]int) int {
 	for i := range dp {
 		dp[i] = make([]int, n)
 	}
-	dp[0][0] = grid[0][0]
-	for i := 1; i < m; i++ {
-		dp[i][0] = dp[i-1][0] + grid[i][0]
-	}
-
-	for j := 1; j < n; j++ {
-		dp[0][j] = dp[0][j-1] + grid[0][j]
-	}
-
-	for i := 1; i < m; i++ {
-		for j := 1; j < n; j++ {
-			dp[i][j] = grid[i][j] + min(dp[i-1][j], dp[i][j-1])
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if i == 0 && j == 0 {
+				dp[i][j] = grid[i][j]
+			} else if i == 0 {
+				dp[i][j] = dp[i][j-1] + grid[i][j]
+			} else if j == 0 {
+				dp[i][j] = dp[i-1][j] + grid[i][j]
+			} else {
+				dp[i][j] = grid[i][j] + min(dp[i-1][j], dp[i][j-1])
+			}
 		}
 	}
 	return dp[m-1][n-1]
