401a6e8f859157ee4fc29de79ff431cf3a6fbf7c
sheltonzhu <498220739@qq.com>
2021-12-29 11:34:17 +0800

    139+

--- 139.word-break/alg.go
+++ 139.word-break/alg.go
@@ -1,5 +1,30 @@
 package _39_word_break
 
 func wordBreak(s string, wordDict []string) bool {
-	return false
+	wordDictSet := make(map[string]bool)
+	maxWordLen := 0
+	for _, w := range wordDict {
+		wordDictSet[w] = true
+		maxWordLen = max(len(w), maxWordLen)
+	}
+	dp := make([]bool, len(s)+1)
+	dp[0] = true
+	for i := 1; i <= len(s); i++ {
+		for j := 0; j < i; j++ {
+			if i-j > maxWordLen {
+				continue
+			}
+			if dp[j] && wordDictSet[s[j:i]] {
+				dp[i] = true
+				break
+			}
+		}
+	}
+	return dp[len(s)]
+}
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
 }
