2e1f5de9e9ec9d1b40cd082561e872ff2011c4da
sheltonzhu <498220739@qq.com>
2022-01-05 20:11:54 +0800

    206

---
+++ 206.reverse-linked-list/alg.go
@@ -0,0 +1,19 @@
+package _06_reverse_linked_list
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseList(head *ListNode) *ListNode {
+	var prev *ListNode
+	curr := head
+	for curr != nil {
+		next := curr.Next
+		curr.Next = prev
+		prev = curr
+		curr = next
+	}
+	return prev
+
+}
---
+++ 206.reverse-linked-list/alg_test.go
@@ -0,0 +1,33 @@
+package _06_reverse_linked_list
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test(t *testing.T) {
+	head := &ListNode{1, &ListNode{2, &ListNode{3, &ListNode{4, &ListNode{5, nil}}}}}
+	output := &ListNode{1, &ListNode{2, &ListNode{3, &ListNode{4, &ListNode{5, nil}}}}}
+	ret := reverseList(head)
+	if !reflect.DeepEqual(output, ret) {
+		t.Errorf("failed. output: %v, return: %v", output, ret)
+	}
+}
+
+func Test2(t *testing.T) {
+	head := &ListNode{1, &ListNode{2, nil}}
+	output := &ListNode{2, &ListNode{1, nil}}
+	ret := reverseList(head)
+	if !reflect.DeepEqual(output, ret) {
+		t.Errorf("failed. output: %v, return: %v", output, ret)
+	}
+}
+
+func Test3(t *testing.T) {
+	head := &ListNode{}
+	output := &ListNode{}
+	ret := reverseList(head)
+	if !reflect.DeepEqual(output, ret) {
+		t.Errorf("failed. output: %v, return: %v", output, ret)
+	}
+}
