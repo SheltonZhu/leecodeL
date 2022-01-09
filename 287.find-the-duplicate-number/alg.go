fb9ba4c9617594cca7ce7c2efe6679b318aa527f
SheltonZhu <498220739@qq.com>
2022-01-09 23:07:30 +0800

    287

--- 287.find-the-duplicate-number/alg.go
+++ 287.find-the-duplicate-number/alg.go
@@ -1,13 +1,23 @@
 package _87_find_the_duplicate_number
 
+//func findDuplicate(nums []int) int {
+//	slow, fast := 0, 0
+//	for slow, fast = nums[slow], nums[nums[fast]]; slow != fast; slow, fast = nums[slow], nums[nums[fast]] {
+//	}
+//	slow = 0
+//	for slow != fast {
+//		slow = nums[slow]
+//		fast = nums[fast]
+//	}
+//	return slow
+//}
 func findDuplicate(nums []int) int {
-	slow, fast := 0, 0
-	for slow, fast = nums[slow], nums[nums[fast]]; slow != fast; slow, fast = nums[slow], nums[nums[fast]] {
+	numMap := map[int]bool{}
+	for _, num := range nums {
+		if _, ok := numMap[num]; ok {
+			return num
+		}
+		numMap[num] = true
 	}
-	slow = 0
-	for slow != fast {
-		slow = nums[slow]
-		fast = nums[fast]
-	}
-	return slow
+	return 0
 }
