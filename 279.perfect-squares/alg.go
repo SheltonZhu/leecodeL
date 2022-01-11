fbe27d41500bff0e6be54326f18c5376989ee664
sheltonzhu <498220739@qq.com>
2022-01-11 11:10:53 +0800

    279x

---
+++ 279.perfect-squares/alg.go
@@ -0,0 +1,26 @@
+package _79_perfect_squares
+
+import (
+	"fmt"
+	"math"
+)
+
+func numSquares(n int) int {
+	f := make([]int, n+1)
+	for i := 1; i <= n; i++ {
+		minn := math.MaxInt32
+		for j := 1; j*j <= i; j++ {
+			minn = min(minn, f[i-j*j])
+			fmt.Printf("i: %d, i-j2: %d, minn: %d \n", i, i-j*j, minn)
+		}
+		f[i] = minn + 1
+	}
+	return f[n]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
